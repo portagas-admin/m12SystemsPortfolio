f100ae1a96b09640e7ebd063acecef85f75f591c
Michael Wassermann <mikew194@protonmail.com>
2024-04-30 21:58:42 -0500

    Updated

---
+++ m12SystemsPortfolio/m12SystemsPortfolio.Client/Models/SkillItem.cs
@@ -0,0 +1,9 @@
+﻿namespace m12SystemsPortfolio.Client.Models
+{
+    public class SkillItem
+    {
+        public int Id { get; set; }
+        public string Skill { get; set; } = "Undefined";
+        public string Description { get; set; } = "Undefined";
+    }
+}
---
+++ m12SystemsPortfolio/m12SystemsPortfolio.Client/Models/WorkItem.cs
@@ -0,0 +1,12 @@
+﻿namespace m12SystemsPortfolio.Client.Models
+{
+    public class WorkItem
+    {
+        public int Id { get; set; }
+        public string Project { get; set; } = "Undefined";
+        public DateOnly StartDate { get; set; } = DateOnly.FromDateTime(DateTime.Now);
+        public DateOnly EndDate { get; set; } = DateOnly.FromDateTime(DateTime.Now);
+        public string Description { get; set; } = "Undefined";
+        public bool Active { get; set; } = false;
+    }
+}
